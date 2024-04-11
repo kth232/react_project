dedf25297035e8d382d392f9c69fb4618d075781
kth232 <kth7537@naver.com>
2024-04-11 18:01:29 +0900

    스타일

---
+++ src/styles/color.js
@@ -0,0 +1,9 @@
+
+export const buttonColor={
+    primary: [color.primary, '#fff'],
+    secondary: [color.secondary, '#fff'],
+    success: [color.success, '#fff'],
+danger: [color.danger, '#fff'],
+warning: [color.warning, '#fff'],
+light: ['#f8f9fa'],
+};
