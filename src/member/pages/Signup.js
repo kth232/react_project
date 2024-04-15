1c6134915e474598550791f2953f80144ae27255
kth232 <kth7537@naver.com>
2024-04-15 17:50:42 +0900

    회원가입 페이지 구성 중

--- src/member/pages/Signup.js
+++ src/member/pages/Signup.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next';
+import SignupForm from '../components/SignupForm';
+import { OuterBox } from '../../commons/components/LayoutBox';
 
 const Signup = () => {
-  return <h1>Sighup page</h1>;
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <Helmet>
+        <title>{t('회원가입')}</title>
+      </Helmet>
+      <OuterBox>
+        <h1>{t('회원가입')}</h1>
+        <SignupForm />
+      </OuterBox>
+    </>
+  ); /*제목도 공통 스타일로 분리해서 만들 예정*/
 };
 
 export default React.memo(Signup);
